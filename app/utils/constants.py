6525041329af2616127bdffffa5fdff806e20e66
galitz-matt <galitz.matthew@gmail.com>
2025-01-06 15:12:25 -0500

    Save SEAS Faculty Directory links per Department

---
+++ app/utils/constants.py
@@ -0,0 +1,12 @@
+SEAS_DEPARTMENTS = {
+    "Biomedical Engineering": "https://engineering.virginia.edu/department/biomedical-engineering/people?keyword=&position=2&impact_area=All&research_area=All&page=",
+    "Chemical Engineering": "https://engineering.virginia.edu/department/chemical-engineering/people?keyword=&position=2&impact_area=All&research_area=All&page=",
+    "Civil and Environmental Engineering": "https://engineering.virginia.edu/department/civil-and-environmental-engineering/people?keyword=&position=2&impact_area=All&research_area=All&page=",
+    "Computer Engineering": "https://engineering.virginia.edu/offices-programs/computer-engineering-program/faculty?keyword=&position=2&impact_area=All&research_area=All&page=",
+    "Computer Science": "https://engineering.virginia.edu/department/computer-science/people&page=",
+    "Electrical and Computer Engineering": "https://engineering.virginia.edu/department/electrical-and-computer-engineering/faculty?keyword=&position=2&impact_area=All&research_area=All&page=",
+    "Engineering and Society": "https://engineering.virginia.edu/department/engineering-and-society/people?keyword=&position=2&impact_area=All&research_area=All&page=",
+    "Materials Science and Engineering": "https://engineering.virginia.edu/department/materials-science-and-engineering/people?impact_area=All&keyword=&position=2&research_area=All&page=",
+    "Mechanical and Aerospace Engineering": "https://engineering.virginia.edu/department/mechanical-and-aerospace-engineering/people?keyword=&position=2&impact_area=All&research_area=All&page=",
+    "Systems and Information Engineering": "https://engineering.virginia.edu/department/systems-and-information-engineering/people?keyword=&position=2&impact_area=All&research_area=All&page="
+}
