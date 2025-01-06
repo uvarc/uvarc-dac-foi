e445ac3097534da4b49aefe4e5e695edf196d949
galitz-matt <galitz.matthew@gmail.com>
2025-01-06 18:32:06 -0500

    Restructure institutional data

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,16 +1,54 @@
-DEPARTMENT_BASE_URLS = {
-    "SEAS": "https://engineering.virginia.edu/"
+SCHOOL_DEPARTMENT_DATA = {
+    "SEAS": {
+        "base_url": "https://engineering.virginia.edu",
+        "departments": {
+            "Biomedical Engineering": {
+                "people_url": "https://engineering.virginia.edu/department/biomedical-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
+            },
+            "Chemical Engineering": {
+                "people_url": "https://engineering.virginia.edu/department/chemical-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
+            },
+            "Civil and Environmental Engineering": {
+                "people_url": ""
+            },
+            "Computer Engineering": {
+                "people_url": "https://engineering.virginia.edu/department/civil-and-environmental-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
+            },
+            "Computer Science": {
+                "people_url": "https://engineering.virginia.edu/department/computer-science/people"
+            },
+            "Electrical and Computer Engineering": {
+                "people_url": "https://engineering.virginia.edu/department/electrical-and-computer-engineering/faculty?keyword=&position=2&impact_area=All&research_area=All"
+            },
+            "Engineering and Society": {
+                "people_url": "https://engineering.virginia.edu/department/engineering-and-society/people?keyword=&position=2&impact_area=All&research_area=All",
+            },
+            "Materials Science and Engineering": {
+                "people_url": "https://engineering.virginia.edu/department/materials-science-and-engineering/people?impact_area=All&keyword=&position=2&research_area=All",
+            },
+            "Mechanical and Aerospace Engineering": {
+                "people_url": "https://engineering.virginia.edu/department/mechanical-and-aerospace-engineering/people?keyword=&position=2&impact_area=All&research_area=All"
+            },
+            "Systems and Information Engineering": {
+                "people_url": "https://engineering.virginia.edu/department/systems-and-information-engineering/people?keyword=&position=2&impact_area=All&research_area=All=",
+            },
+        },
+    },
 }
 
-SEAS_DEPARTMENT_PEOPLE = {
-    "Biomedical Engineering": "https://engineering.virginia.edu/department/biomedical-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
-    "Chemical Engineering": "https://engineering.virginia.edu/department/chemical-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
-    "Civil and Environmental Engineering": "https://engineering.virginia.edu/department/civil-and-environmental-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
-    "Computer Engineering": "https://engineering.virginia.edu/offices-programs/computer-engineering-program/faculty?keyword=&position=2&impact_area=All&research_area=All",
-    "Computer Science": "https://engineering.virginia.edu/department/computer-science/people",
-    "Electrical and Computer Engineering": "https://engineering.virginia.edu/department/electrical-and-computer-engineering/faculty?keyword=&position=2&impact_area=All&research_area=All",
-    "Engineering and Society": "https://engineering.virginia.edu/department/engineering-and-society/people?keyword=&position=2&impact_area=All&research_area=All",
-    "Materials Science and Engineering": "https://engineering.virginia.edu/department/materials-science-and-engineering/people?impact_area=All&keyword=&position=2&research_area=All",
-    "Mechanical and Aerospace Engineering": "https://engineering.virginia.edu/department/mechanical-and-aerospace-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
-    "Systems and Information Engineering": "https://engineering.virginia.edu/department/systems-and-information-engineering/people?keyword=&position=2&impact_area=All&research_area=All="
+
+SCHOOL_DEPARTMENT_PEOPLE_URL = {
+    "SEAS": {
+        "Biomedical Engineering": "https://engineering.virginia.edu/department/biomedical-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
+        "Chemical Engineering": "https://engineering.virginia.edu/department/chemical-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
+        "Civil and Environmental Engineering": "https://engineering.virginia.edu/department/civil-and-environmental-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
+        "Computer Engineering": "https://engineering.virginia.edu/offices-programs/computer-engineering-program/faculty?keyword=&position=2&impact_area=All&research_area=All",
+        "Computer Science": "https://engineering.virginia.edu/department/computer-science/people",
+        "Electrical and Computer Engineering": "https://engineering.virginia.edu/department/electrical-and-computer-engineering/faculty?keyword=&position=2&impact_area=All&research_area=All",
+        "Engineering and Society": "https://engineering.virginia.edu/department/engineering-and-society/people?keyword=&position=2&impact_area=All&research_area=All",
+        "Materials Science and Engineering": "https://engineering.virginia.edu/department/materials-science-and-engineering/people?impact_area=All&keyword=&position=2&research_area=All",
+        "Mechanical and Aerospace Engineering": "https://engineering.virginia.edu/department/mechanical-and-aerospace-engineering/people?keyword=&position=2&impact_area=All&research_area=All",
+        "Systems and Information Engineering": "https://engineering.virginia.edu/department/systems-and-information-engineering/people?keyword=&position=2&impact_area=All&research_area=All=",
+    }
+
 }
