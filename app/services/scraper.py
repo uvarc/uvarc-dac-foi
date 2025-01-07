585cf7303e01780a120d0cf07bd01d000c55ac72
galitz-matt <galitz.matthew@gmail.com>
2025-01-07 15:49:21 -0500

    Extract About Section from faculty profiles

--- app/services/scraper.py
+++ app/services/scraper.py
@@ -26,8 +26,8 @@ class ProfileScraper:
         page_number = 0
         profile_urls = []
         MAX_PAGES = 100
-        NO_RESULTS_DIV = '//div[contains(@class, "results_message_inner typography") and contains(text(), "There are no results matching these criteria.")]'
-        CONTACT_BLOCK_NAME_A_TAG = '//a[contains(@class, "contact_block_name_link")]/@href'
+        NO_RESULTS_XPATH = '//div[contains(@class, "results_message_inner typography") and contains(text(), "There are no results matching these criteria.")]'
+        CONTACT_BLOCK_NAME_XPATH = '//a[contains(@class, "contact_block_name_link")]/@href'
 
         while page_number < MAX_PAGES:
             page_url = f"{people_url}&page={page_number}"
@@ -44,11 +44,11 @@ class ProfileScraper:
 
             try:
                 tree = html.fromstring(response.content)
-                no_results = tree.xpath(NO_RESULTS_DIV)
+                no_results = tree.xpath(NO_RESULTS_XPATH)
                 if no_results:
                     logger.info(f"No results found for page {page_number}: {page_url}")
                     break
-                urls = tree.xpath(CONTACT_BLOCK_NAME_A_TAG)
+                urls = tree.xpath(CONTACT_BLOCK_NAME_XPATH)
                 profile_urls.extend(urls)
                 page_number += 1
             except html.etree.XMLSyntaxError as e:
@@ -68,22 +68,58 @@ class ProfileScraper:
             logger.error(f'Invalid URL: {profile_url}')
             return []
 
-        EMAIL_A_TAG = "//a[contains(@class, 'people_meta_detail_info_link') and starts-with(@href, 'mailto:')]/@href"
-
         try:
             response = self.http_client.request('GET', profile_url)
         except Exception:
             return []
 
+        EMAIL_XPATH = "//a[contains(@class, 'people_meta_detail_info_link') and starts-with(@href, 'mailto:')]/@href"
         try:
             tree = html.fromstring(response.content)
-            raw_emails = tree.xpath(EMAIL_A_TAG)
+            raw_emails = tree.xpath(EMAIL_XPATH)
             emails = {email.replace("mailto:", "").strip() for email in raw_emails}
             return list(emails)
         except html.etree.XMLSyntaxError as e:
             logger.error(f"Failed to parse HTML for {profile_url}: {e}")
         except Exception as e:
             logger.error(f"Unexpected error processing page {profile_url}: {e}")
+
+
+    def get_about_from_profile(self, profile_url: str) -> typing.List[str]:
+        """
+        Extracts about from profile URLs.
+        :param profile_url: the URL to the profile page
+        :return: About Section text for profile
+        """
+        if not InstitutionUtils.is_valid_url(profile_url):
+            logger.error(f'Invalid URL: {profile_url}')
+            return []
+
+        try:
+            response = self.http_client.request('GET', profile_url)
+        except Exception:
+            return []
+
+        EDUCATION_XPATH = "//h2[text()='Education']"
+        ABOUT_AND_EDUCATION_XPATH = "//h2[text()='About']/following-sibling::*[following-sibling::h2[text()='Education']]"
+        ABOUT_XPATH = "//h2[text()='About']/following-sibling::*"
+
+        try:
+            tree = html.fromstring(response.content)
+            raw_education = tree.xpath(EDUCATION_XPATH)
+            if raw_education:
+                raw_about = tree.xpath(ABOUT_AND_EDUCATION_XPATH)
+            else:
+                raw_about = tree.xpath(ABOUT_XPATH)
+            about_content = [element.text_content().strip() for element in raw_about if element.text_content().strip()]
+            if about_content:
+                logger.info(f"Extract About section text for profile: {profile_url}")
+                return about_content
+            else:
+                logger.warning(f"No About section text found for profile: {profile_url}")
+                return []
+        except Exception:
+            return []
 
 
 http_client = HttpClient()
@@ -93,5 +129,4 @@ biomed_profile_endpoints = scraper.get_profile_endpoints_from_people_page(biomed
 endpoint = biomed_profile_endpoints[0]
 seas_base_url = InstitutionUtils.get_school_base_url("SEAS")
 profile_url = InstitutionUtils.get_profile_url(seas_base_url, endpoint)
-emails = scraper.get_emails_from_profile(profile_url)
-print(emails)
+text = scraper.get_about_from_profile(profile_url)
