7736584b74ef8a5afb35568f8a1d13b71b3a141f
galitz-matt <galitz.matthew@gmail.com>
2025-01-08 20:59:01 -0500

    Avoid multiple list mutations within loop

--- app/services/scraper_service.py
+++ app/services/scraper_service.py
@@ -25,39 +25,21 @@ class ScraperService:
 
         logger.info(f"Extracting faculty profile endpoints from {department} webpage")
         profile_endpoints = scraper.get_profile_endpoints_from_people(people_url)
-        faculty_names = []
-        faculty_emails = []
-        faculty_about = []
-        faculty_profile_urls = []
 
+        faculty_data = []
         for endpoint in profile_endpoints:
-            profile_url = InstitutionUtils.get_profile_url(school_base_url, endpoint)
-            faculty_profile_urls.append(profile_url)
+            profile_url = InstitutionUtils.get_profile_url(endpoint)
+            name = scraper.get_name_from_profile(profile_url) or "UNKNOWN"
+            emails = ", ".join(scraper.get_emails_from_profile(profile_url)) or ["UNKNOWN"]
+            about = scraper.get_about_from_profile(profile_url) or "UNKNOWN"
 
-            name = scraper.get_name_from_profile(profile_url)
-            if self._data_not_found(name):
-                logger.warning(f"Name not found for URL: {profile_url}")
-                name = "UNKNOWN"
-            faculty_names.append(name)
+            faculty_data.append({
+                "Faculty Name": name,
+                "Email Address": emails,
+                "About Section": about,
+                "Profile URL": profile_url,
+            })
 
-            emails = scraper.get_emails_from_profile(profile_url)
-            if self._data_not_found(emails):
-                logger.warning(f"Email not found for URL: {profile_url}")
-                emails = ["UNKNOWN"]
-            faculty_emails.append(emails)
-
-            about = scraper.get_about_from_profile(profile_url)
-            if self._data_not_found(about):
-                logger.warning(f"About not found for URL: {profile_url}")
-                about = "UNKNOWN"
-            faculty_about.append(about)
-
-        faculty_data = {
-            "Faculty Name": faculty_names,
-            "About Section": faculty_about,
-            "Email Address": faculty_emails,
-            "Profile URL": faculty_profile_urls,
-        }
         return pd.DataFrame(faculty_data)
 
     def _select_scraper(self, department: str) -> BaseScraper:
@@ -67,21 +49,23 @@ class ScraperService:
         :return: scraper instance corresponding to given department
         """
         school_id = InstitutionUtils.get_school_from_department(department)
-        return next(scraper for scraper in self.scrapers if scraper.SCHOOL_ID == school_id)
+        for scraper in self.scrapers:
+            if scraper.SCHOOL_ID == school_id:
+                return scraper
+        raise ValueError(f"No scraper found for department {department}")
 
-    def _data_not_found(self, object: typing.Any) -> bool:
-        if isinstance(object, str):
-            return object == ""
-        if isinstance(object, list):
-            return object == []
+    def _data_not_found(self, obj: typing.Union[str, typing.List]) -> bool:
+        if isinstance(obj, str):
+            return obj == ""
+        if isinstance(obj, list):
+            return obj == []
 
 
 if __name__ == '__main__':
     from app.utils.http_client import HttpClient
-    from app.utils.institution_utils import InstitutionUtils
     from app.services.scraper import SEASScraper
 
     seas_scraper = SEASScraper(HttpClient())
     scrapers = [seas_scraper]
     service = ScraperService(scrapers)
-    print(service.get_department_info('Biomedical Engineering'))
+    print(service.get_department_faculty_data('Biomedical Engineering'))
