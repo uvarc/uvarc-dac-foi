8f1bfdb8d2e98fff4d69728827dff9a8e4dba31c
galitz-matt <galitz.matthew@gmail.com>
2025-01-09 16:14:56 -0500

    Fix spelling error

--- app/models/models.py
+++ app/models/models.py
@@ -4,11 +4,13 @@ db = SQLAlchemy()
 
 class School(db.Model):
     __tablename__ = "school"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
 
 class Department(db.Model):
     __tablename__ = "department"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     school_id = db.Column(db.Integer, db.ForeignKey("school.id"), nullable=False)
@@ -16,6 +18,7 @@ class Department(db.Model):
 
 class Faculty(db.Model):
     __tablename__ = "faculty"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     email = db.relationship('FacultyEmail', backref=db.backref("faculty", lazy=True, cascade="all, delete-orphan"))
@@ -25,7 +28,8 @@ class Faculty(db.Model):
     department = db.relationship('Department', backref=db.backref("faculty", lazy=True))
 
 class FacultyEmail(db.Model):
-    __table_name__ = "faculty_email"
+    __tablename__ = "faculty_email"
+
     email = db.Column(db.String(300), primary_key=True)
     faculty_id = db.Column(db.Integer, db.ForeignKey("faculty.id"), nullable=False)
     faculty = db.relationship('Faculty', backref=db.backref("emails", lazy=True))
