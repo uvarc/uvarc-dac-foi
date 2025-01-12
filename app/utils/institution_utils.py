bcb5928960c889e6247ee15e97e1e6479b0b1529
galitz-matt <galitz.matthew@gmail.com>
2025-01-12 17:25:57 -0500

    Add logging and error handling

--- app/utils/institution_utils.py
+++ app/utils/institution_utils.py
@@ -1,28 +1,44 @@
 import re
 import typing
+import logging
 
 from app.core.constants import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class InstitutionUtils:
     @staticmethod
     def get_departments_from_school(school: str) -> typing.List[str]:
-        return [dept for dept in SCHOOL_DEPARTMENT_DATA[school]["departments"]]
+        try:
+            return [dept for dept in SCHOOL_DEPARTMENT_DATA[school]["departments"]]
+        except KeyError:
+            logger.error(f"No departments found for school: {school}")
+            raise
 
     @staticmethod
     def get_school_from_department(department: str) -> str:
         for school, data in SCHOOL_DEPARTMENT_DATA.items():
             if department in data["departments"]:
                 return school
-        return "UNKNOWN"
+        raise RuntimeError(f"No school found for department: {department}")
 
     @staticmethod
     def get_school_base_url(school: str) -> str:
-        return SCHOOL_DEPARTMENT_DATA[school]["base_url"]
+        try:
+            return SCHOOL_DEPARTMENT_DATA[school]["base_url"]
+        except KeyError:
+            logger.error(f"No school found for school: {school}")
+            raise
 
     @staticmethod
     def get_people_url_from_department(department: str) -> str:
-        school = InstitutionUtils.get_school_from_department(department)
-        return SCHOOL_DEPARTMENT_DATA[school]["departments"][department]["people_url"]
+        try:
+            school = InstitutionUtils.get_school_from_department(department)
+            return SCHOOL_DEPARTMENT_DATA[school]["departments"][department]["people_url"]
+        except KeyError:
+            logger.error(f"No people URL found for department: {department}")
+            raise
 
     @staticmethod
     def make_profile_url(base_url: str, profile_endpoint: str) -> str:
