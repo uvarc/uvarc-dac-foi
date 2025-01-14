18faac5b94c3ff07a5b68db33e2287afb98e2183
galitz-matt <galitz.matthew@gmail.com>
2025-01-13 19:37:31 -0500

    Build NIH RePORTER API wrapper

--- app/services/nih/nih_reporter_proxy.py
+++ app/services/nih/nih_reporter_proxy.py
@@ -1,5 +1,13 @@
+import typing
+import logging
+import copy
+from datetime import datetime
+from requests import RequestException, Timeout, HTTPError
 from app.utils.http_client import HttpClient
 from app.core.constants import NIH_REPORTER_PAYLOAD
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class NIHReporterProxy():
     NIH_REPORTER_ENDPOINT = "https://api.reporter.nih.gov/v2/projects/search"
@@ -7,3 +15,37 @@ class NIHReporterProxy():
     def __init__(self, http_client: HttpClient):
         self.http_client = http_client
 
+    def build_payload(self, first_name: str, last_name: str, fiscal_years: typing.List[int] = None) -> typing.Dict:
+        """
+        Build the payload for the NIH RePORTER API request
+        :param first_name: PI's first name
+        :param last_name: PI's last name
+        :param fiscal_years: list of fiscal years to filter results
+        :return: payload as dictionary
+        """
+        if fiscal_years is None:
+            fiscal_years = [datetime.now().year]
+
+        payload = copy.deepcopy(NIH_REPORTER_PAYLOAD)
+        payload["criteria"]["pi_names"]["first_name"] = first_name
+        payload["criteria"]["pi_names"]["last_name"] = last_name
+        payload["criteria"]["fiscal_years"] = fiscal_years
+        return payload
+
+    def call_reporter_api(self, payload: typing.Dict) -> typing.Dict:
+        """
+        Call the NIH RePORTER API with the given payload
+        :param payload: the payload for the POST request
+        :return: API response as a dictionary
+        :raises: Any exceptions raised by the HTTP client
+        """
+        logger.info(f"Invoking NIH RePORTER API with payload: {payload}")
+        try:
+            response = self.http_client.post(self.NIH_REPORTER_ENDPOINT, json=payload)
+            return response.json()
+        except (Timeout, HTTPError, RequestException, ValueError) as e
+            logger.error(f"NIH Reporter API request failed with error:{e}"
+                         f"Please verify endpoint correctness: {self.NIH_REPORTER_ENDPOINT}")
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            raise
