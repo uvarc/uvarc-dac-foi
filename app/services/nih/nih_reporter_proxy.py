ee28d04caf5f809ca796ab3211ed72f8eb9d32af
galitz-matt <galitz.matthew@gmail.com>
2025-01-14 20:36:16 -0500

    Refactor build payload functionality to service

--- app/services/nih/nih_reporter_proxy.py
+++ app/services/nih/nih_reporter_proxy.py
@@ -1,9 +1,7 @@
 import typing
 import logging
-import copy
 from requests import RequestException, Timeout, HTTPError
 from app.utils.http_client import HttpClient
-from app.core.constants import NIH_REPORTER_PAYLOAD
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -14,20 +12,6 @@ class NIHReporterProxy:
     def __init__(self, http_client: HttpClient):
         self.http_client = http_client
 
-    @staticmethod
-    def build_payload(first_name: str, last_name: str, fiscal_years: typing.List) -> typing.Dict:
-        """
-        Build the payload for the NIH RePORTER API request
-        :param first_name: PI's first name
-        :param last_name: PI's last name
-        :param fiscal_years: list of fiscal years to filter results
-        :return: payload as dictionary
-        """
-        payload = copy.deepcopy(NIH_REPORTER_PAYLOAD)
-        payload["criteria"]["pi_names"][0]["first_name"] = first_name
-        payload["criteria"]["pi_names"][0]["last_name"] = last_name
-        payload["criteria"]["fiscal_years"] = fiscal_years
-        return payload
 
     def call_reporter_api(self, payload: typing.Dict) -> typing.Dict:
         """
