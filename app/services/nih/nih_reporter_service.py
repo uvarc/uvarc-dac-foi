c98533ab033f558db33e978792ef0610ab949e6e
galitz-matt <galitz.matthew@gmail.com>
2025-01-16 17:37:16 -0500

    Change method signature to enhance parameter understandability

--- app/services/nih/nih_reporter_service.py
+++ app/services/nih/nih_reporter_service.py
@@ -16,18 +16,17 @@ class NIHReporterService:
     def __init__(self, proxy: NIHReporterProxy):
         self.proxy = proxy
 
-    def get_project_data(self, first_name: str = None, last_name: str = None, fiscal_years: typing.List = None) -> pd.DataFrame:
-        if first_name is None or last_name is None:
+    def get_project_data(self, pi_first_name: str = None, pi_last_name: str = None, fiscal_years: typing.List = None) -> pd.DataFrame:
+        if pi_first_name is None or pi_last_name is None:
             raise ValueError("")
         if fiscal_years is None:
             fiscal_years = [datetime.now().year]
 
-        payload = self.build_payload(first_name, last_name, fiscal_years)
+        payload = self.build_payload(pi_first_name, pi_last_name, fiscal_years)
         response = self.proxy.call_reporter_api(payload)
-        print(response)
         projects = self.get_all_projects(response)
 
-        all_project_data = []
+        compiled_metadata = []
         for project in projects:
             metadata = dict()
             metadata["project_number"] = self.get_project_number(project)
@@ -62,17 +61,17 @@ class NIHReporterService:
             return "N/A"
 
     @staticmethod
-    def build_payload(first_name: str, last_name: str, fiscal_years: typing.List) -> typing.Dict:
+    def build_payload(pi_first_name: str, pi_last_name: str, fiscal_years: typing.List) -> typing.Dict:
         """
         Build the payload for the NIH RePORTER API request
-        :param first_name: PI's first name
-        :param last_name: PI's last name
+        :param pi_first_name: PI's first name
+        :param pi_last_name: PI's last name
         :param fiscal_years: list of fiscal years to filter results
         :return: payload as dictionary
         """
         payload = copy.deepcopy(NIH_REPORTER_PAYLOAD)
-        payload["criteria"]["pi_names"][0]["first_name"] = first_name
-        payload["criteria"]["pi_names"][0]["last_name"] = last_name
+        payload["criteria"]["pi_names"][0]["first_name"] = pi_first_name
+        payload["criteria"]["pi_names"][0]["last_name"] = pi_last_name
         payload["criteria"]["fiscal_years"] = fiscal_years
         return payload
 
