f6704f4849ac43c9ac4ff48ddbb1c1a60104064d
galitz-matt <galitz.matthew@gmail.com>
2025-01-21 20:14:33 -0500

    Inject OpenAI client

--- app/services/embedding/embedding_generator.py
+++ app/services/embedding/embedding_generator.py
@@ -1,18 +1,16 @@
-from openai import OpenAI
-
-client = OpenAI(api_key=api_key)
 import logging
 import typing
+from openai import OpenAI
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class EmbeddingGenerator:
-    def __init__(self, api_key: str):
-        logging.info("Initialized EmbeddingGenerator with OpenAI API key")
+    def __init__(self, client: OpenAI):
+        self.client = client
+        logging.info("Initialized EmbeddingGenerator with OpenAI client")
 
-    @staticmethod
-    def generate_embedding(text: str) -> typing.List[float]:
+    def generate_embedding(self, text: str) -> typing.List[float]:
         """
         Generates an embedding for provided text using OpenAI's text-embedding-ada-002 model
         :param text: input text
@@ -20,7 +18,7 @@ class EmbeddingGenerator:
         """
         logging.info(f"Generating embedding for text: {text}")
         try:
-            response = client.embeddings.create(input=text,
+            response = self.client.embeddings.create(input=text,
             model="text-embedding-ada-002")
             logging.info("Successfully generated embedding.")
             return response.data[0].embedding
