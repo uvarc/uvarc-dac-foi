e53d344004f6fa0560696f56b03e5343dddc1c43
galitz-matt <galitz.matthew@gmail.com>
2025-01-27 19:40:35 -0500

    Set up app configuration as object

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = os.path.join(os.path.dirname(__file__), "../../rc-DACFOI.env")
+load_dotenv(dotenv_path=env_path)
 
 class Config:
-    secret_key = os.getenv("SECRET_KEY")
-    sqlalchemy_database_uri = os.getenv("SQLALCHEMY_DATABASE_URI")
-    sqlalchemy_track_modifications = False
-    static_folder = "../static"
-    templates_folder = "../templates"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STATIC_FOLDER = "../static"
+    TEMPLATES_FOLDER = "../templates"
