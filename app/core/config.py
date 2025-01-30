289e5541dbeba2c4ff61b4367e4b1c80474f8384
galitz-matt <galitz.matthew@gmail.com>
2025-01-29 21:05:52 -0500

    Populate database w/ Faculty records

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ env_path = os.path.join(os.path.dirname(__file__), "../../rc-DACFOI.env")
 load_dotenv(dotenv_path=env_path)
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    OPENAI_API_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = "../static"
