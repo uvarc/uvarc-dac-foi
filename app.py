b1e0300a989fe904550c2265365daae077689e34
galitz-matt <galitz.matthew@gmail.com>
2025-02-02 15:33:37 -0500

    Inject SearchService into app factory

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from backend.core import create_app
+from backend.utils.factory import search_service
 
-app = create_app()
+search_service_instance = search_service()
+app = create_app(search_service_instance=search_service_instance)
 
 if __name__ == '__main__':
     app.run()
