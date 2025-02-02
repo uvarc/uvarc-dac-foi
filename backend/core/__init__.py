ed62c76a0195adfc689df56ffacb198abf527190
galitz-matt <galitz.matthew@gmail.com>
2025-02-02 15:23:37 -0500

    Instantiate SearchService at app creation

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from backend.utils.factory import search_service
+from backend.views.search_view import create_search_blueprint
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -9,11 +11,11 @@ def create_app(config_class="backend.core.config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    search_service_instance = search_service()
+    search_bp = create_search_blueprint(search_service_instance)
+    app.register_blueprint(search_bp, url_prefix="/api/search")
+
     db.init_app(app)
     migrate.init_app(app, db)
-
-    from backend.models.models import Faculty, Project
-    with app.app_context():
-        db.create_all()
 
     return app
