320130629c523e27ad424b557d6ac382bc074d90
galitz-matt <galitz.matthew@gmail.com>
2025-02-02 15:31:12 -0500

    Inject SearchService into app factory

--- backend/core/__init__.py
+++ backend/core/__init__.py
@@ -2,17 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from backend.core.config import Config
+from backend.services.search.search_service import SearchService
 from backend.utils.factory import search_service
 from backend.views.search_view import create_search_blueprint
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(config_class=Config)
+def create_app(config_class=Config, search_service_instance: SearchService=None):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    search_service_instance = search_service()
+    if not search_service_instance:
+        search_service_instance = search_service()
+
     search_bp = create_search_blueprint(search_service_instance)
     app.register_blueprint(search_bp, url_prefix="/api/search")
 
