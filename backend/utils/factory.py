cfe7a64bce9b4670b2027536333c4fbbdac7186f
galitz-matt <galitz.matthew@gmail.com>
2025-02-02 15:22:17 -0500

    Abstract DI w/ factory

---
+++ backend/utils/factory.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from backend.core.config import Config
+from backend.services.database.database_driver import DatabaseDriver
+from backend.services.embedding.embedding_generator import EmbeddingGenerator
+from backend.services.embedding.embedding_service import EmbeddingService
+from backend.services.embedding.embedding_storage import EmbeddingStorage
+from backend.services.embedding.preprocessor import Preprocessor
+from backend.services.search.search_service import SearchService
+
+def search_service():
+    openai_client = OpenAI(api_key=Config.OPENAI_API_KEY)
+    preprocessor = Preprocessor()
+    embedding_storage = EmbeddingStorage()
+    embedding_generator = EmbeddingGenerator(openai_client)
+    embedding_service = EmbeddingService(preprocessor, embedding_generator, embedding_storage)
+    database_driver = DatabaseDriver()
+    return SearchService(database_driver, embedding_service)
