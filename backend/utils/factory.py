41b899178746e1265b11260b50e8efc5f420e6e9
galitz-matt <galitz.matthew@gmail.com>
2025-02-04 19:45:51 -0500

    Provide all API dependencies

--- backend/utils/factory.py
+++ backend/utils/factory.py
@@ -1,15 +1,30 @@
 from openai import OpenAI
-from backend.core.config import Config
-from backend.services.database.database_driver import DatabaseDriver
-from backend.services.embedding.embedding_generator import EmbeddingGenerator
-from backend.services.embedding.embedding_service import EmbeddingService
-from backend.services.embedding.embedding_storage import EmbeddingStorage
-from backend.services.search.search_service import SearchService
 
-def search_service():
-    openai_client = OpenAI(api_key=Config.OPENAI_API_KEY)
-    embedding_storage = EmbeddingStorage()
-    embedding_generator = EmbeddingGenerator(openai_client)
-    embedding_service = EmbeddingService(embedding_generator, embedding_storage)
-    database_driver = DatabaseDriver()
+def get_openai_client():
+    from backend.core.config import Config
+    return OpenAI(api_key=Config.OPENAI_API_KEY)
+
+def get_embedding_generator(client: OpenAI):
+    from backend.services.embedding.embedding_service import EmbeddingGenerator
+    return EmbeddingGenerator(client)
+
+def get_embedding_storage():
+    from backend.services.embedding.embedding_storage import EmbeddingStorage
+    return EmbeddingStorage()
+
+def get_embedding_service():
+    from backend.services.embedding.embedding_service import EmbeddingService
+    return EmbeddingService(
+        embedding_generator=get_embedding_generator(get_openai_client()),
+        embedding_storage=get_embedding_storage(),
+    )
+
+def get_database_driver():
+    from backend.services.database.database_driver import DatabaseDriver
+    return DatabaseDriver()
+
+def get_search_service():
+    from backend.services.search.search_service import SearchService
+    embedding_service = get_embedding_service()
+    database_driver = get_database_driver()
     return SearchService(database_driver, embedding_service)
