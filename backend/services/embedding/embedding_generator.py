0aaa7c4a387e8eb66c3f7821fded456336d712a7
galitz-matt <galitz.matthew@gmail.com>
2025-02-04 19:31:46 -0500

    Lazy import token_utils functions

--- backend/services/embedding/embedding_generator.py
+++ backend/services/embedding/embedding_generator.py
@@ -2,7 +2,6 @@ import logging
 import typing
 from openai import OpenAI
 from backend.core.script_config import OPENAI_CONFIG
-from backend.utils.token_utils import count_tokens, chunk_text
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,6 +19,8 @@ class EmbeddingGenerator:
         :return: embedding
         """
         logger.info(f"Generating embedding for text: {text}")
+
+        from backend.utils.token_utils import count_tokens
         token_count = count_tokens(text)
         if token_count <= OPENAI_CONFIG["MAX_TOKENS"]:
             return self._call_embedding_api(text)
@@ -33,6 +34,7 @@ class EmbeddingGenerator:
         :param text: input text
         :return: aggregated embedding
         """
+        from backend.utils.token_utils import chunk_text
         chunks = chunk_text(text)
 
         embeddings = []
