7c89dd1f8b74b469837b4c2752d54043ab5f2e6f
galitz-matt <galitz.matthew@gmail.com>
2025-02-06 17:18:40 -0500

    Add paths to templates and static folders

--- backend/core/config.py
+++ backend/core/config.py
@@ -8,5 +8,5 @@ class Config:
     OPENAI_API_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STATIC_FOLDER = "../../frontend/static"
-    TEMPLATES_FOLDER = "../../frontend/templates"
+    STATIC_FOLDER = "../../frontend/static/"
+    TEMPLATES_FOLDER = "../../frontend/templates/"
