2f0eae6aec0bf659dcdb3a602cd8f306f620536f
galitz-matt <galitz.matthew@gmail.com>
2025-02-06 16:59:53 -0500

    Create blueprint from home UI

---
+++ backend/views/search_ui_view.py
@@ -0,0 +1,12 @@
+import os
+from flask import Blueprint, render_template
+from backend.core.config import Config
+
+search_template = os.path.join(Config.TEMPLATES_FOLDER, "search.html")
+
+def create_search_ui_blueprint():
+    search_ui_bp = Blueprint('search_ui', __name__)
+
+    @search_ui_bp.route("/")
+    def search_ui():
+        return render_template(search_template)
