6ea880bac4a32519e5164be44edd13868c5bd6ab
galitz-matt <galitz.matthew@gmail.com>
2025-02-06 17:19:19 -0500

    Fix pathing to search template

--- backend/views/search_ui_view.py
+++ backend/views/search_ui_view.py
@@ -1,14 +1,10 @@
-import os
 from flask import Blueprint, render_template
-from backend.core.config import Config
-
-search_template = os.path.join(Config.TEMPLATES_FOLDER, "search.html")
 
 def create_search_ui_blueprint():
     search_ui_bp = Blueprint('search_ui', __name__)
 
     @search_ui_bp.route("/")
     def search_ui():
-        return render_template(search_template)
+        return render_template("search.html")
 
     return search_ui_bp
