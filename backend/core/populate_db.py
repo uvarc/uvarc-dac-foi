8632602874f71a713871c8b122a692af38632eb1
galitz-matt <galitz.matthew@gmail.com>
2025-02-16 16:47:49 -0500

    Add log statements

--- backend/core/populate_db.py
+++ backend/core/populate_db.py
@@ -1,3 +1,4 @@
+import logging
 from backend.core.script_config import SCHOOLS_TO_SCRAPE
 from backend.utils.http_client import HttpClient
 from backend.utils.factory import get_embedding_service, get_database_driver
@@ -8,6 +9,7 @@ from backend.services.nih.nih_reporter_service import NIHReporterService
 from backend.services.aggregator.data_aggregator import DataAggregator
 from app import app
 
+logger = logging.getLogger(__name__)
 
 # instantiate dependencies
 http_client = HttpClient()
@@ -16,15 +18,19 @@ seas_scraper = SEASScraper(http_client)
 scraper_service = ScraperService([seas_scraper])
 nih_service = NIHReporterService(NIHReporterProxy(http_client))
 embedding_service = get_embedding_service()
+database_driver = get_database_driver(app=app)
 
-data_aggregator = DataAggregator(scraper_service, nih_service, embedding_service)
+data_aggregator = DataAggregator(scraper_service, nih_service, embedding_service, database_driver)
 
 if __name__ == '__main__':
+    logger.info("Starting populate_db.")
     all_faculty = []
+
+    logger.info("Clearing database.")
+    database_driver.clear()
+
     for school in SCHOOLS_TO_SCRAPE:
         all_faculty.extend(data_aggregator.aggregate_school_faculty_data(school))
 
-    database_driver = get_database_driver(app=app)
-    database_driver.clear()
     for faculty in all_faculty:
-        database_driver.add_or_update_faculty(faculty)
+        database_driver.add_faculty(faculty)
